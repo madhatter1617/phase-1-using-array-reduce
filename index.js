f62da9bc6cce8ca5d773f778c8db16091be1611a
Rosa <rosarenee@att.net>
2021-07-02 10:37:44 -0500

    commit tree 3a3ff7f3b4c0fadaf01923573d59681650fbff10

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = 0;
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log("Biss test");
+totalBatteries = batteryBatches.reduce(reducer);
